0bf64edbcd8a7b91008e34aa99f2171edc10f848
Nuno Monteiro <nunomo@microsoft.com>
2021-01-03 12:12:49 +0000

    design alignment

--- AzureStorageLogReader/SALogReaderGui.Designer.cs
+++ AzureStorageLogReader/SALogReaderGui.Designer.cs
@@ -40,6 +40,7 @@
             this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.panelLeft = new System.Windows.Forms.Panel();
+            this.spinningCirclesConnectionPane = new AzureStorageLogReader.SpinningCircles();
             this.treeView = new System.Windows.Forms.TreeView();
             this.panelMain = new System.Windows.Forms.Panel();
             this.rbtnClassic = new System.Windows.Forms.RadioButton();
@@ -51,13 +52,12 @@
             this.txtFilter = new System.Windows.Forms.TextBox();
             this.lblFilter = new System.Windows.Forms.Label();
             this.btnChooseColumns = new System.Windows.Forms.Button();
+            this.spinningCircles = new AzureStorageLogReader.SpinningCircles();
             this.btbClearTable = new System.Windows.Forms.Button();
             this.lblNumberRows = new System.Windows.Forms.Label();
             this.dataGridView = new System.Windows.Forms.DataGridView();
             this.btnAddLogs = new System.Windows.Forms.Button();
             this.openFileDialogJson = new System.Windows.Forms.OpenFileDialog();
-            this.spinningCircles = new AzureStorageLogReader.SpinningCircles();
-            this.spinningCirclesConnectionPane = new AzureStorageLogReader.SpinningCircles();
             this.menuStrip1.SuspendLayout();
             this.panelLeft.SuspendLayout();
             this.panelMain.SuspendLayout();
@@ -116,7 +116,6 @@
             // 
             // panelLeft
             // 
-            this.panelLeft.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.panelLeft.Controls.Add(this.spinningCirclesConnectionPane);
             this.panelLeft.Controls.Add(this.treeView);
             this.panelLeft.Dock = System.Windows.Forms.DockStyle.Left;
@@ -124,6 +123,16 @@
             this.panelLeft.Name = "panelLeft";
             this.panelLeft.Size = new System.Drawing.Size(420, 608);
             this.panelLeft.TabIndex = 15;
+            // 
+            // spinningCirclesConnectionPane
+            // 
+            this.spinningCirclesConnectionPane.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.spinningCirclesConnectionPane.BackColor = System.Drawing.Color.Transparent;
+            this.spinningCirclesConnectionPane.Location = new System.Drawing.Point(155, 255);
+            this.spinningCirclesConnectionPane.Name = "spinningCirclesConnectionPane";
+            this.spinningCirclesConnectionPane.Size = new System.Drawing.Size(90, 90);
+            this.spinningCirclesConnectionPane.TabIndex = 19;
+            this.spinningCirclesConnectionPane.Text = "spinningCircles1";
             // 
             // treeView
             // 
@@ -136,13 +145,12 @@
             treeNode2.Text = "Connections";
             this.treeView.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
             treeNode2});
-            this.treeView.Size = new System.Drawing.Size(418, 606);
+            this.treeView.Size = new System.Drawing.Size(420, 608);
             this.treeView.TabIndex = 0;
             this.treeView.DoubleClick += new System.EventHandler(this.treeView_DoubleClick);
             // 
             // panelMain
             // 
-            this.panelMain.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.panelMain.Controls.Add(this.rbtnClassic);
             this.panelMain.Controls.Add(this.rbtnNew);
             this.panelMain.Controls.Add(this.bntExporToExcel);
@@ -190,9 +198,9 @@
             // bntExporToExcel
             // 
             this.bntExporToExcel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.bntExporToExcel.Location = new System.Drawing.Point(17, 572);
+            this.bntExporToExcel.Location = new System.Drawing.Point(19, 566);
             this.bntExporToExcel.Name = "bntExporToExcel";
-            this.bntExporToExcel.Size = new System.Drawing.Size(150, 26);
+            this.bntExporToExcel.Size = new System.Drawing.Size(150, 34);
             this.bntExporToExcel.TabIndex = 25;
             this.bntExporToExcel.Text = "Export To Excel";
             this.bntExporToExcel.UseVisualStyleBackColor = true;
@@ -201,7 +209,7 @@
             // btnClearFilter
             // 
             this.btnClearFilter.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnClearFilter.Location = new System.Drawing.Point(577, 41);
+            this.btnClearFilter.Location = new System.Drawing.Point(579, 41);
             this.btnClearFilter.Name = "btnClearFilter";
             this.btnClearFilter.Size = new System.Drawing.Size(52, 28);
             this.btnClearFilter.TabIndex = 24;
@@ -221,7 +229,7 @@
             // btnFilter
             // 
             this.btnFilter.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnFilter.Location = new System.Drawing.Point(513, 41);
+            this.btnFilter.Location = new System.Drawing.Point(515, 41);
             this.btnFilter.Name = "btnFilter";
             this.btnFilter.Size = new System.Drawing.Size(62, 28);
             this.btnFilter.TabIndex = 22;
@@ -236,7 +244,7 @@
             this.txtFilter.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.1F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txtFilter.Location = new System.Drawing.Point(456, 42);
             this.txtFilter.Name = "txtFilter";
-            this.txtFilter.Size = new System.Drawing.Size(54, 25);
+            this.txtFilter.Size = new System.Drawing.Size(56, 25);
             this.txtFilter.TabIndex = 21;
             // 
             // lblFilter
@@ -251,20 +259,30 @@
             // btnChooseColumns
             // 
             this.btnChooseColumns.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnChooseColumns.Location = new System.Drawing.Point(170, 572);
+            this.btnChooseColumns.Location = new System.Drawing.Point(172, 566);
             this.btnChooseColumns.Name = "btnChooseColumns";
-            this.btnChooseColumns.Size = new System.Drawing.Size(150, 26);
+            this.btnChooseColumns.Size = new System.Drawing.Size(150, 34);
             this.btnChooseColumns.TabIndex = 19;
             this.btnChooseColumns.Text = "Choose Columns";
             this.btnChooseColumns.UseVisualStyleBackColor = true;
             this.btnChooseColumns.Click += new System.EventHandler(this.btnChooseColumns_Click);
             // 
+            // spinningCircles
+            // 
+            this.spinningCircles.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.spinningCircles.BackColor = System.Drawing.Color.Transparent;
+            this.spinningCircles.Location = new System.Drawing.Point(282, 255);
+            this.spinningCircles.Name = "spinningCircles";
+            this.spinningCircles.Size = new System.Drawing.Size(90, 90);
+            this.spinningCircles.TabIndex = 18;
+            this.spinningCircles.Text = "spinningCircles1";
+            // 
             // btbClearTable
             // 
             this.btbClearTable.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.btbClearTable.Location = new System.Drawing.Point(324, 572);
+            this.btbClearTable.Location = new System.Drawing.Point(326, 566);
             this.btbClearTable.Name = "btbClearTable";
-            this.btbClearTable.Size = new System.Drawing.Size(150, 26);
+            this.btbClearTable.Size = new System.Drawing.Size(150, 34);
             this.btbClearTable.TabIndex = 17;
             this.btbClearTable.Text = "Clear Table";
             this.btbClearTable.UseVisualStyleBackColor = true;
@@ -289,15 +307,15 @@
             this.dataGridView.Name = "dataGridView";
             this.dataGridView.RowHeadersWidth = 51;
             this.dataGridView.RowTemplate.Height = 24;
-            this.dataGridView.Size = new System.Drawing.Size(618, 487);
+            this.dataGridView.Size = new System.Drawing.Size(620, 483);
             this.dataGridView.TabIndex = 15;
             // 
             // btnAddLogs
             // 
             this.btnAddLogs.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnAddLogs.Location = new System.Drawing.Point(478, 572);
+            this.btnAddLogs.Location = new System.Drawing.Point(480, 566);
             this.btnAddLogs.Name = "btnAddLogs";
-            this.btnAddLogs.Size = new System.Drawing.Size(152, 26);
+            this.btnAddLogs.Size = new System.Drawing.Size(152, 34);
             this.btnAddLogs.TabIndex = 14;
             this.btnAddLogs.Text = "Add Log files";
             this.btnAddLogs.UseVisualStyleBackColor = true;
@@ -310,26 +328,6 @@
             this.openFileDialogJson.Multiselect = true;
             this.openFileDialogJson.RestoreDirectory = true;
             this.openFileDialogJson.SupportMultiDottedExtensions = true;
-            // 
-            // spinningCircles
-            // 
-            this.spinningCircles.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.spinningCircles.BackColor = System.Drawing.Color.Transparent;
-            this.spinningCircles.Location = new System.Drawing.Point(281, 254);
-            this.spinningCircles.Name = "spinningCircles";
-            this.spinningCircles.Size = new System.Drawing.Size(90, 90);
-            this.spinningCircles.TabIndex = 18;
-            this.spinningCircles.Text = "spinningCircles1";
-            // 
-            // spinningCirclesConnectionPane
-            // 
-            this.spinningCirclesConnectionPane.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.spinningCirclesConnectionPane.BackColor = System.Drawing.Color.Transparent;
-            this.spinningCirclesConnectionPane.Location = new System.Drawing.Point(154, 254);
-            this.spinningCirclesConnectionPane.Name = "spinningCirclesConnectionPane";
-            this.spinningCirclesConnectionPane.Size = new System.Drawing.Size(90, 90);
-            this.spinningCirclesConnectionPane.TabIndex = 19;
-            this.spinningCirclesConnectionPane.Text = "spinningCircles1";
             // 
             // SALogReaderGui
             // 
