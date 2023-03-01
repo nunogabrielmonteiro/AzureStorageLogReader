ca558ad7c85be9628ea812b9c44c1447a7ab88f5
Thakur Prasad Mishra <tmishra@microsoft.com>
2023-03-01 07:49:58 +0530

    Added form to choose container in preview

---
+++ AzureStorageLogReader/ChooseForm.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AzureStorageLogReader
+{
+    public partial class ChooseForm : Form
+    {
+        public int mySelectedIndex = 0;
+
+        public ChooseForm()
+        {
+            InitializeComponent();
+        }
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            mySelectedIndex = 0;
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            mySelectedIndex = 1;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.OK;
+            this.Close();
+
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            mySelectedIndex = 2;
+        }
+    }
+}
